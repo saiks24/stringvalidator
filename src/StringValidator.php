245b140c4b90ee6b55db4cfe329f61ed521f076e
System Administrator <root@pukpuk.local>
2018-01-06 18:30:24 +0300

    Init commit

---
+++ src/StringValidator.php
@@ -0,0 +1,30 @@
+<?php namespace Saiks;
+
+
+class StringValidator
+{
+    public static function validateString(String $inputString) : bool {
+
+        if(empty($inputString)) {
+            throw new \InvalidArgumentException('Empty argument');
+        }
+
+        $re = '/[^() \n\r\t]/';
+        $matches = preg_match_all($re, $inputString, $matches, PREG_SET_ORDER, 0);
+        if(!empty($matches)) {
+            throw new \InvalidArgumentException('The argument contains invalid characters');
+        }
+
+        $counter = 0;
+        $inputCharsArray = str_split($inputString);
+        foreach ($inputCharsArray as $char) {
+            if($char === '(') {
+                $counter++;
+            } elseif ($char === ')') {
+                $counter--;
+            }
+        }
+
+        return $counter !== 0;
+    }
+}
